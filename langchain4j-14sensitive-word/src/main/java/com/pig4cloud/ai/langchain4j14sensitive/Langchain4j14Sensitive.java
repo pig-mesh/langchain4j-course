0460d5e418ed42f4a5b1f0c2f9b6dc3e732d6401
冷冷 <2270033969@qq.com>
2024-10-09 12:52:14 +0800

    feat(14): 敏感词

---
+++ langchain4j-14sensitive-word/src/main/java/com/pig4cloud/ai/langchain4j14sensitive/Langchain4j14Sensitive.java
@@ -0,0 +1,13 @@
+package com.pig4cloud.ai.langchain4j14sensitive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Langchain4j14Sensitive {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Langchain4j14Sensitive.class, args);
+    }
+
+}
